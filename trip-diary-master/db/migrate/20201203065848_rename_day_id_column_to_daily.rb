f3222ce0d0d08658217dfc7de9f9d568d86371f1
terrine-18kip <73570560+terrine-18kip@users.noreply.github.com>
2021-01-09 04:42:04 +0900

    first commit

--- trip-diary-master/db/migrate/20201203065848_rename_day_id_column_to_daily.rb
+++
@@ -1,5 +0,0 @@
-class RenameDayIdColumnToDaily < ActiveRecord::Migration[6.0]
-  def change
-    rename_column :plans, :day_id, :daily
-  end
-end
--- trip-diary-master/db/migrate/20201222092019_rename_row_order_column_to_spots.rb
+++
@@ -1,5 +0,0 @@
-class RenameRowOrderColumnToSpots < ActiveRecord::Migration[6.0]
-  def change
-    rename_column :spots, :row_order, :position
-  end
-end
